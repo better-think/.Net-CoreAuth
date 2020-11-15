0532bdf5cbbd6e0027e783d5e4b3644ebb19ac08
chaojin752 <65781166+chaojin752@users.noreply.github.com>
2020-11-15 19:08:27 +0700

    inti

---
+++ NetCoreAuth.App/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NetCoreAuth.App
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme,
+                    options =>
+                    {
+                        options.LoginPath = new PathString("/auth/login");
+                        options.AccessDeniedPath = new PathString("/auth/denied");
+                    });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
---
+++ NetCoreJWTAuth.App/Controllers/ApiController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace NetCoreJWTAuth.App.Controllers
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api")]
+    public class ApiController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public ApiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet("Test")]
+        public IActionResult Test()
+        {
+            var userName = User.Identity.Name;
+
+            return Ok($"Super secret content, I hope you've got clearance for this {userName}...");
+        }
+
+        [Authorize(Policy = "TrainedStaffOnly")]
+        [HttpPost("DeleteUser")]
+        public IActionResult DeleteUser(string username)
+        {
+            // go wild, delete the user, do what you have to...
+            return Ok("Deleted");
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] TokenRequest request)
+        {
+            if (request.Username == "Jon" && request.Password == "Again, not for production use, DEMO ONLY!")
+            {
+                var claims = new[]
+                {
+                    new Claim(ClaimTypes.Name, request.Username),
+                    new Claim("CompletedBasicTraining", ""),
+                    new Claim(CustomClaimTypes.EmploymentCommenced,
+                                new DateTime(2017,12,1).ToString(),
+                                ClaimValueTypes.DateTime)
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SecurityKey"]));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "yourdomain.com",
+                    audience: "yourdomain.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+
+    }
+
+    public class TokenRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
